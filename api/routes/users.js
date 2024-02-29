d570c1629128ad68ada5526655a1b3b4cca723da
Manassawinzz <s6404062610341@email.kmutnb.ac.th>
2024-03-01 05:02:30 +0700

    new push

--- api/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { fetchUserData, addNewPort} = require('../controllers/users.js')
-const { authenticateToken} = require('../controllers/auth.js')
-const router = express()
-router.get('/fetchUserData', authenticateToken, fetchUserData);
-router.post("/addPort",authenticateToken, addNewPort);
-
-
-module.exports =  router;
