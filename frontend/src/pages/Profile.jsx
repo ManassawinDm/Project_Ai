ab320f74c0a4c67c7cd0c4d28fb966ef62fe30f9
Manassawinzz <s6404062610341@email.kmutnb.ac.th>
2024-03-01 04:59:42 +0700

    gghfg

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
-
+import { AuthContext } from '../component/authContext';
+import axios from "axios"
 
 function Profile() {
     const navigate = useNavigate();
@@ -8,6 +9,63 @@ function Profile() {
     const handlePay=()=>{
         navigate("/pay")
     }
+    const [message, setmessage] = useState(null)
+    const [userEmail, setuserEmail] = useState(null);
+  const { authToken } = useContext(AuthContext);
+  const [portNumbers, setPortNumbers] = useState([]); // Example port numbers
+  const [selectedPortNumber, setSelectedPortNumber] = useState('');
+  const [NewPort, setNewPort] = useState('');
+
+  const handleAddPortChange = (event) => {
+    setNewPort(event.target.value);
+  };
+
+  const handlePortNumberChange = (event) => {
+    // console.log("HERE " ,event.target.value)
+    setSelectedPortNumber(event.target.value);
+    // console.log(selectedPortNumber)
+  };
+
+  const submitchange = async () => {
+    try {
+      await axios.post('http://localhost:8800/api/user/addPort', { portNumber: NewPort }, {
+        headers: {
+          Authorization: `Bearer ${authToken}`
+        }
+      });
+      console.log('Port added successfully');
+      setPortNumbers(prevPortNumbers => [...prevPortNumbers, NewPort]);
+      setNewPort('');
+      setmessage('Port added successfully')
+    } catch (error) {
+      setmessage(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8800/api/user/fetchUserData', {
+          headers: {
+            Authorization: `Bearer ${authToken}`
+          }
+        });
+        // console.log(response.data.userData.port_numbers)
+        setuserEmail(response.data.userData.email);
+        setPortNumbers(response.data.userData.port_numbers);
+        console.log(response.data.userData.port_numbers)
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    };
+    if (authToken) {
+      fetchUserData();
+    }
+  }, [authToken]);
+  if (!userEmail) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex justify-center p-4 mt-8">
       {/* Wrapper for both forms with a max-width and centered */}
@@ -24,18 +82,14 @@ function Profile() {
                 {/* Camera icon or edit icon suggested */}
               </button>
             </div>
-            <form className="space-y-6">
-              <input
-                className="w-full px-4 py-3 border rounded-lg"
-                type="text"
-                placeholder="Name"
-              />
+            <div className="space-y-6">
               <input
                 className="w-full px-4 py-3 border rounded-lg"
                 type="email"
-                placeholder="Email"
+                placeholder={userEmail}
+                disabled
               />
-              <input
+              {/* <input
                 className="w-full px-4 py-3 border rounded-lg"
                 type="password"
                 placeholder="Password"
@@ -44,31 +98,54 @@ function Profile() {
                 className="w-full px-4 py-3 border rounded-lg"
                 type="password"
                 placeholder="Confirm Password"
-              />
-              <button className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out">
+              /> */}
+               <div className="mb-6">
+          <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="email">
+            Add port
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3"
+            id="add port"
+            type="text"
+            placeholder="add port number"
+            value={NewPort} 
+            onChange={handleAddPortChange}
+
+          />
+        </div>
+        <div className="flex items-start mb-6 text-red-500 justify-center">
+            {message}
+          </div>
+              <button className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out"  onClick = { () => submitchange()}>
                 Save changes
               </button>
-            </form>
+            </div>
           </div>
 
           {/* New Form */}
           <div className=" flex items-center bg-white rounded-lg shadow-lg p-8 flex-grow ml-20 ">
             <form className="space-y-6">
-              <select
-                className="w-full px-4 py-3 border rounded-lg"
-                defaultValue=""
-              >
-                <option value="" disabled>
-                  Select your Ports
-                </option>
-                <option value="user1">user1</option>
-                <option value="user2">user2</option>
-                <option value="user3">user3</option>
-              </select>
+            <select
+            id="portNumber"
+            className="w-full px-4 py-3 border rounded-lg"
+            value={selectedPortNumber}
+            onChange={handlePortNumberChange}
+          >
+
+            {portNumbers.map((portNumber, index) => (
+              <option key={index} value={portNumber}>
+                {portNumber}
+              </option>
+            ))}
+          </select>
+          <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="email">
+            Commission
+          </label>
               <input
                 className="w-full px-4 py-3 border rounded-lg"
                 type="tel"
-                placeholder="Phone Number"
+                placeholder="10.356 Bath"
+                disabled
               />
               <button onClick={handlePay} className=" w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out  ">
                 Pay Commission
